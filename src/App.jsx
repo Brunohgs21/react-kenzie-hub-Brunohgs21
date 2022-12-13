28bfc9acdaef2d28a4ed5717ba4cd67dbc78cc7d
Brunohgs21 <brunohgs21@outlook.com>
2022-12-12 21:16:49 -0300

    att

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Global from "./styles/global";
 import RoutesMain from "./routes";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <>
       <Global />
+      <AuthProvider>
+
       <RoutesMain />
+      </AuthProvider>
     </>
   );
 }
---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+export const AuthContext = createContext({})
+
+export const AuthProvider = () =>{
+
+    return(
+        <AuthContext.Provider>
+
+        </AuthContext.Provider>
+    )
+
+}
